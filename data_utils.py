3dd0b2eb3a3ac8aa14f17b9b1176403f0d87ded9
Daniel Serezane <serezane@protonmail.com>
2023-01-07 03:36:43 -0300

    est n hidden

--- data_utils.py
+++ data_utils.py
@@ -1,12 +1,11 @@
 # Funções auxiliares para manipulação de dados.
-# Abrem o conjunto de treinamento, vê a quantidade de entradas e saídas,
-# estima o número de neurônios na camada oculta e embaralham os dados.
-
 import pandas as pd
 import numpy as np
 
 SEED = 666
 
+# Abre o conjunto de treinamento, vê a quantidade de entradas e saídas,
+# estima o número de neurônios na camada oculta e embaralham os dados.
 def import_data(data_path):
     data = pd.read_csv(data_path)
     # Embaralha os dados para que o treinamento seja mais eficiente.
@@ -17,4 +16,6 @@ def import_data(data_path):
     inputs = data.iloc[:, :-1].values
     input_size = inputs.shape[1]
 
-    return inputs, targets, input_size, output_size
+    est_n_hidden = int(np.sqrt(input_size * output_size))
+
+    return inputs, targets, input_size, output_size, est_n_hidden
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from NeuralNetwork import NeuralNetwork
 from data_utils import import_data
 
-inputs, targets, input_size, output_size = import_data('data/treinamento.csv')
+inputs, targets, input_size, output_size, n_hidden = import_data('data/treinamento.csv')
 
-nn = NeuralNetwork(inputs, targets, input_size, output_size, 5)
+nn = NeuralNetwork(inputs, targets, input_size, output_size, n_hidden)
 nn.train(1000)
 cm = nn.test('data/teste.csv')
 print(cm)
