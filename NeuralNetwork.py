c46433eceaaf56b0619825bc75cb14b0a6094e10
GuiCT <gtomiasi@gmail.com>
2023-01-14 14:34:53 -0300

    atualizando função de forward e backpropag

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,6 +6,7 @@ from utils import *
 class NeuralNetwork:
     def __init__(self, inputs, targets, input_size, output_size, n_hidden, stop_criteria = 'epochs', act_func = 'tanh'):
         np.random.seed(666)
+        inputs = (inputs - inputs.min()) / (inputs.max() - inputs.min())
         self.inputs = inputs
         self.targets = prepare_targets(targets, act_func, output_size)
         self.input_size = input_size
@@ -13,8 +14,10 @@ class NeuralNetwork:
         self.n_hidden_layers = n_hidden
         self.stop_criteria = stop_criteria
 
-        self.weights1 = np.random.rand(self.input_size, self.n_hidden_layers)
-        self.weights2 = np.random.rand(self.n_hidden_layers, self.output_size)
+        # self.weights1 = np.random.rand(self.input_size, self.n_hidden_layers)
+        # self.weights2 = np.random.rand(self.n_hidden_layers, self.output_size)
+        self.weights1 = np.random.uniform(-0.01, 0.01, (self.input_size, self.n_hidden_layers))
+        self.weights2 = np.random.uniform(-0.01, 0.01, (self.n_hidden_layers, self.output_size))
 
         if act_func == 'tanh':
             self.act_func = tanh
@@ -30,26 +33,24 @@ class NeuralNetwork:
         self.output = self.act_func(net_output)
         return self.output
 
-    def __backward(self, targets):
+    def __backward(self, inputs, targets):
         self.errors = targets - self.output
         self.output_deltas = self.errors * self.act_func_derivative(self.output)
         self.errors_hidden = self.output_deltas @ self.weights2.T
         self.hidden_deltas = self.errors_hidden * self.act_func_derivative(self.hidden_layers)
-        self.weights2 += self.hidden_layers.T @ self.output_deltas
-        self.weights1 += self.inputs.T @ self.hidden_deltas
+        self.weights2 += np.vstack(self.hidden_layers) @ np.reshape(self.output_deltas, (-1, self.output_deltas.shape[0]))
+        self.weights1 += np.vstack(inputs) @ np.reshape(self.hidden_deltas, (-1, self.hidden_deltas.shape[0]))
 
     def train(self, stop_value):
-        inputs = [self.inputs]
-        targets = [self.targets]
         if self.stop_criteria == 'epochs':
             for _ in range(stop_value):
-                for input, target in zip(inputs, targets):
+                for input, target in zip(self.inputs, self.targets):
                     self.forward(input)
-                    self.__backward(target)
+                    self.__backward(input, target)
         elif self.stop_criteria == 'error':
             error = error_function(self.targets, self.forward(self.inputs))
             while error > stop_value:
-                for input, target in zip(inputs, targets):
+                for input, target in zip(self.inputs, self.targets):
                     self.forward(input)
                     self.__backward(target)
                     error = error_function(target, self.output)
@@ -57,7 +58,7 @@ class NeuralNetwork:
     def test(self, data_path):
         data = pd.read_csv(data_path)
         # A última coluna é o target
-        targets = data.iloc[:, -1].values
+        targets = prepare_targets(data.iloc[:, -1].values)
         inputs = data.iloc[:, :-1].values
         predictions = self.forward(inputs)
         predictions = np.argmax(predictions, axis=1) + 1
